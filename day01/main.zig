eecbd96152c332d60399b6a0f13f7a5b6b33a93b
Ching-Feng Wang <chingfeng.wang@gmail.com>
2024-12-14 14:36:59 +0800

    first commit

---
+++ day01/main.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+
+const example = @embedFile("example.txt");
+//const example2 = @embedFile("example2.txt");
+const input = @embedFile("input.txt");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    try std.testing.expectEqual(puzzle1(allocator, example), 11);
+    const part1 = try puzzle1(allocator, input);
+    std.debug.print("part1 = {d}\n", .{part1});
+    try std.testing.expectEqual(puzzle2(example), 31);
+    const part2 = try puzzle2(input);
+    std.debug.print("part2 = {d}\n", .{part2});
+}
+
+test "puzzle 1" {
+    const allocator = std.testing.allocator;
+    try std.testing.expectEqual(puzzle1(allocator, example), 11);
+}
+
+test "puzzle 2" {
+    try std.testing.expectEqual(puzzle2(example), 31);
+}
+
+fn puzzle1(allocator: std.mem.Allocator, puzzle: []const u8) !usize {
+    var lines = std.mem.splitScalar(u8, puzzle, '\n');
+
+    var first_list = std.ArrayList(usize).init(allocator);
+    var second_list = std.ArrayList(usize).init(allocator);
+    defer {
+        first_list.deinit();
+        second_list.deinit();
+    }
+
+    while (lines.next()) |line| {
+        if (line.len == 0) break;
+        var it = std.mem.tokenizeAny(u8, line, " ");
+        const first_number = try std.fmt.parseInt(usize, it.next().?, 10);
+        const second_number = try std.fmt.parseInt(usize, it.next().?, 10);
+        // std.debug.print("{d} {d}\n", .{ first_number, second_number });
+        try first_list.append(first_number);
+        try second_list.append(second_number);
+    }
+    std.mem.sort(usize, first_list.items, {}, std.sort.asc(usize));
+    std.mem.sort(usize, second_list.items, {}, std.sort.asc(usize));
+    // std.debug.print("first list = {any}\n", .{first_list});
+    // std.debug.print("second list = {any}\n", .{second_list});
+    var sum: usize = 0;
+    for (0..first_list.items.len) |i| {
+        const first: isize = @intCast(first_list.items[i]);
+        const second: isize = @intCast(second_list.items[i]);
+        sum += @abs(first - second);
+    }
+    return sum;
+}
+
+fn puzzle2(puzzle: []const u8) !usize {
+    var lines = std.mem.splitScalar(u8, puzzle, '\n');
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var first_list = std.ArrayList(usize).init(allocator);
+    var second_list = std.ArrayList(usize).init(allocator);
+    defer {
+        first_list.deinit();
+        second_list.deinit();
+    }
+
+    while (lines.next()) |line| {
+        if (line.len == 0) break;
+        var it = std.mem.tokenizeAny(u8, line, " ");
+        const first_number = try std.fmt.parseInt(usize, it.next().?, 10);
+        const second_number = try std.fmt.parseInt(usize, it.next().?, 10);
+        // std.debug.print("{d} {d}\n", .{ first_number, second_number });
+        try first_list.append(first_number);
+        try second_list.append(second_number);
+    }
+    std.mem.sort(usize, first_list.items, {}, std.sort.asc(usize));
+    std.mem.sort(usize, second_list.items, {}, std.sort.asc(usize));
+    // std.debug.print("first list = {any}\n", .{first_list});
+    // std.debug.print("second list = {any}\n", .{second_list});
+    const first_items = first_list.items;
+    const second_items = second_list.items;
+    const list_len = first_items.len;
+    var sum: usize = 0;
+    var i: usize = 0;
+    var j: usize = 0;
+    while ((i < list_len) and (j < list_len)) {
+        if (first_items[i] < second_items[j]) {
+            i += 1;
+        } else if (first_items[i] > second_items[j]) {
+            j += 1;
+        } else if (first_items[i] == second_items[j]) {
+            const number = first_items[i];
+            var i_next = i + 1;
+            var j_next = j + 1;
+            while ((i_next < list_len) and (first_items[i_next] == number)) {
+                i_next += 1;
+            }
+            while ((j_next < list_len) and (second_items[j_next] == number)) {
+                j_next += 1;
+            }
+            // std.debug.print("number = {d}\n", .{number});
+            // std.debug.print("i = {d} j = {d}\n", .{ i, j });
+            // std.debug.print("i_next {d} j_next = {d}\n", .{ i_next, j_next });
+            sum += number * (i_next - i) * (j_next - j);
+            i = i_next;
+            j = j_next;
+        }
+    }
+    return sum;
+}
---
+++ day02/main.zig
@@ -0,0 +1,112 @@
+const std = @import("std");
+
+const example = @embedFile("example.txt");
+const input = @embedFile("input.txt");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    try std.testing.expectEqual(2, puzzle1(example));
+    const part1 = try puzzle1(input);
+    std.debug.print("part1 = {d}\n", .{part1});
+    try std.testing.expectEqual(4, puzzle2(allocator, example));
+    const part2 = try puzzle2(input);
+    std.debug.print("part2 = {d}\n", .{part2});
+}
+
+fn puzzle1(puzzle: []const u8) !usize {
+    var lines = std.mem.splitScalar(u8, puzzle, '\n');
+
+    var count: usize = 0;
+    while (lines.next()) |line| {
+        if (line.len == 0) break;
+        var it = std.mem.tokenizeAny(u8, line, " ");
+        var level = try std.fmt.parseInt(isize, it.next().?, 10);
+        var level1 = level;
+        level = try std.fmt.parseInt(isize, it.next().?, 10);
+        var level2 = level;
+        var diff = level2 - level1;
+        if ((diff == 0) or (@abs(diff) > 3)) continue;
+        var safe = true;
+        while (it.next()) |level_text| {
+            level = try std.fmt.parseInt(isize, level_text, 10);
+            level1 = level2;
+            level2 = level;
+            const diff1 = level2 - level1;
+            if ((diff1 == 0) or (@abs(diff1) > 3)) {
+                safe = false;
+                break;
+            }
+            if (std.math.sign(diff) != std.math.sign(diff1)) {
+                safe = false;
+                break;
+            }
+            diff = diff1;
+        }
+        if (safe) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+fn is_safe(levels: []isize) bool {
+    var diff = levels[1] - levels[0];
+    if ((diff == 0) or (@abs(diff) > 3)) return false;
+    for (2..levels.len) |i| {
+        const diff1 = levels[i] - levels[i - 1];
+        if ((diff1 == 0) or (@abs(diff1) > 3)) {
+            return false;
+        }
+        if (std.math.sign(diff) != std.math.sign(diff1)) {
+            return false;
+        }
+        diff = diff1;
+    }
+    return true;
+}
+
+fn puzzle2(allocator: std.mem.Allocator, puzzle: []const u8) !usize {
+    var lines = std.mem.splitScalar(u8, puzzle, '\n');
+
+    var levels = std.ArrayList(isize).init(allocator);
+    defer levels.deinit();
+
+    var count: usize = 0;
+    while (lines.next()) |line| {
+        if (line.len == 0) break;
+
+        var it = std.mem.tokenizeAny(u8, line, " ");
+        while (it.next()) |level_text| {
+            const level = try std.fmt.parseInt(isize, level_text, 10);
+            try levels.append(level);
+        }
+        // std.debug.print("line = {s}\n", .{line});
+
+        const len = levels.items.len;
+        const levels_minus_one = try allocator.alloc(isize, len - 1);
+        defer allocator.free(levels_minus_one);
+        for (0..len) |skip| {
+            var j: usize = 0;
+            for (0..len) |i| {
+                if (i != skip) {
+                    levels_minus_one[j] = levels.items[i];
+                    j += 1;
+                }
+            }
+            // std.debug.print("{any}\n", .{levels_minus_one});
+            if (is_safe(levels_minus_one)) {
+                count += 1;
+                break;
+            }
+        }
+        levels.clearRetainingCapacity();
+    }
+    return count;
+}
+
+test "puzzle 2" {
+    const allocator = std.testing.allocator;
+    try std.testing.expectEqual(4, puzzle2(allocator, example));
+}
